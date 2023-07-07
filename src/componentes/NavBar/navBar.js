65b63f2b5fe4eac0bced0292caa6de0b98a482f2
Joaquin Cabrera <joaquincccr@gmail.com>
2023-07-07 00:57:45 -0300

    Detalles generales re-entrega 2, sigue sin descripcion

--- src/componentes/NavBar/navBar.js
+++ src/componentes/NavBar/navBar.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import CartWidget from "../CartWidget/cartWidget.js";
-import "../CSS/NavBar.css"
+import "../CSS/NavBar.css";
 
 function NavBar(){
     return(
